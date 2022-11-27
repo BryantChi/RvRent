456ec52caedb3cfd34bc5cd69a2ca73c830c43e8
BryantChi <bryantchi.work@gmail.com>
2022-11-28 06:13:43 +0800

    [MOD] step-bar 修正位置

--- resources/views/layouts/rent_step_bar.blade.php
+++ resources/views/layouts/rent_step_bar.blade.php
@@ -2,25 +2,25 @@
     <div class="container">
         <div class="row justify-content-center align-content-center px-md-5 px-3 pt-5 pb-md-5 pb-4">
             <div class="col-md-3">
-                <div class="step-box d-flex justify-md-content-center align-items-center step-box-pass ">
+                <div class="step-box d-flex justify-content-md-center align-items-center step-box-pass ">
                     <span class="step-num mr-3">1</span>
                     <p class="step-txt my-auto">選擇您的露營車</p>
                 </div>
             </div>
             <div class="col-md-3">
-                <div class="step-box d-flex justify-md-content-center align-items-center step-box-active">
+                <div class="step-box d-flex justify-content-md-center align-items-center step-box-active">
                     <span class="step-num mr-3">2</span>
                     <p class="step-txt my-auto">選擇加購方案</p>
                 </div>
             </div>
             <div class="col-md-3">
-                <div class="step-box d-flex justify-md-content-center align-items-center step-box-default">
+                <div class="step-box d-flex justify-content-md-center align-items-center step-box-default">
                     <span class="step-num mr-3">3</span>
                     <p class="step-txt my-auto">提交付款</p>
                 </div>
             </div>
             <div class="col-md-3">
-                <div class="step-box d-flex justify-md-content-center align-items-center step-box-default">
+                <div class="step-box d-flex justify-content-md-center align-items-center step-box-default">
                     <span class="step-num mr-3">4</span>
                     <p class="step-txt my-auto">預訂成功</p>
                 </div>
