657034f1d08816dde5ad57e73e3330c77c704baa
BryantChi <Bryantchi.work@gmail.com>
2023-07-17 14:27:22 +0800

    [ADD] 新增契約條款書

--- resources/views/rv_rent_s3.blade.php
+++ resources/views/rv_rent_s3.blade.php
@@ -12,6 +12,7 @@
             </div>
             <div class="row">
 
+                <iframe src="{{ asset('assets/file/租賃條款契約書（0111）-Ranger.pdf') }}" width="100%" height="600px"></iframe>
             </div>
             <div class="row justify-content-center">
                 <a href="{{ route('car_rent_s4') }}" class="btn btn-primary3">下一步</a>
