c9301781225dfeee01f18f69885e919dfd3faaa2
BryantChi <Bryantchi.work@gmail.com>
2023-07-18 10:32:40 +0800

    [MOD] 修正契約書UI

--- resources/views/rv_rent_s3.blade.php
+++ resources/views/rv_rent_s3.blade.php
@@ -11,13 +11,13 @@
                 <h2>露營車租賃契約書</h2>
             </div>
             <div class="row mb-3 mx-md-auto mx-3">
-
-                <iframe src="{{ asset('assets/file/租賃條款契約書（0111）-Ranger.pdf') }}" width="100%"
-                    style="height: 70vh !important;"></iframe>
+                <embed src="{{ asset('assets/file/租賃條款契約書（0111）-Ranger.pdf') }}" type="application/pdf" width="100%"
+                    style="height: 70vh !important;">
             </div>
             <div class="row mb-3 mx-md-auto mx-3">
                 <div class="form-group form-check form-control-lg d-flex align-items-center">
-                    <input type="checkbox" class="form-check-input" name="readed" id="readed">
+                    <input type="checkbox" class="form-check-input" name="readed"
+                        id="readed" value="*請選擇“同意”繼續操作*">
                     <label class="form-check-label" style="font-size: 1.2rem !important;" for="readed">*請選擇“同意”繼續操作*</label>
                 </div>
             </div>
@@ -27,17 +27,20 @@
         </div>
     </section>
     <script>
-        $('.btn-next').prop('disabled', true);
+        $('.btn-next').attr('disabled', true);
         $('.btn-next').addClass('btn-secondary');
-        if ($('#readed').is(":checked")) {
-            $('#readed').prop('disabled', true)
-            $('.btn-next').prop('disabled', false);
-            $('.btn-next').removeClass('btn-secondary');
-            $('.btn-next').addClass('btn-primary3');
-        }
+        $('#readed').click(function() {
+            if ($('#readed').is(":checked")) {
+                $('#readed').attr('disabled', true)
+                $('.btn-next').attr('disabled', false);
+                $('.btn-next').removeClass('btn-secondary');
+                $('.btn-next').addClass('btn-primary3');
+            }
+        });
+
 
         $('.btn-next').click(function() {
-            window.location.href="{{ route('car_rent_s4') }}";
+            window.location.href = "{{ route('car_rent_s4') }}";
         });
     </script>
 @endsection
