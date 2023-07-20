2f6c9c9a5e3b4db6f99bf5ca36cbb62f3e0414c4
BryantChi <Bryantchi.work@gmail.com>
2023-07-20 16:48:09 +0800

    [MOD] 後台UI調整

--- app/Admin/Controllers/RvModelInfoController.php
+++ app/Admin/Controllers/RvModelInfoController.php
@@ -112,55 +112,64 @@ class RvModelInfoController extends AdminController
     protected function form()
     {
         return Form::make(new RvModelInfo(), function (Form $form) {
-            $form->display('id');
-            $form->text('rv_name')->required();
-            $form->image('rv_front_cover')->move('images/rv/' . date('Ym') . '/frontCover')->uniqueName()->maxSize(1024)->rules('mimes:jpg,jpeg,png,gif')->required();
-            $form->select('rv_series_id')->options(RvSeries::pluck('rv_series_name', 'id'))->required();
-
-            $form->checkbox('attachment_id')->options(RvAttachment::orderBy('order')->pluck('attachment_name', 'id'))->canCheckAll();
-
-            if ($form->isCreating()) {
-                $val_status = 0;
-            } else {
-                $val_status = $form->model()->stock;
-            }
-            $form->display('stock')->value($val_status);
-            $form->number('bed_count');
-            $form->currency('base_price')->symbol('$');
-
-            $form->array('rv_rent_setting', function ($form) {
-                $form->select('week', __('起始星期'))->options([0 => '星期日', 1 => '星期一', 2 => '星期二', 3 => '星期三', 4 => '星期四', 5 => '星期五', 6 => '星期六']);
-                $form->time('get', __('取車時間'))->format('HH:00');
-                $form->time('back', __('還車時間'))->format('HH:00');
-                $form->number('day', __('可租天數(夜)'));
-                $form->currency('rental', __('租金費率/天(夜)'))->symbol('$');
-                $form->currency('overtime', __('超租加收/天(夜)'))->symbol('$');
-                $form->table('other_price', __('其他收費'), function ($table) {
-                    $table->text('item', __('項目'));
-                    $table->currency('price', __('價格'))->symbol('$');
-                    $table->select('type', __('計算方式'))->options(['night' => '天', 'times' => '次'])->default('night');
-                })->saving(function ($v) {
-                    return json_encode($v);
-                });
-                $form->currency('milage', __('里程數/夜'))->symbol('km');
-                $form->text('note', __('備註'));
-                // $form->switch('plan', __(''));
-                $form->radio('plan', __('設為方案'))
-                    ->options([1 => '是', 0 => '否'])
-                    ->default(0)
-                    ->when(1, function (NestedForm $form) {
-                        $form->text('plan_title', __('方案名稱'));
-                    });
-
-            })->saveAsJson();
 
 
-            $form->editor('rv_discription')->options(['menubar' => false, 'toolbar' => ['code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | \
+            $form->tab('Basic', function (Form $form) {
+                $form->display('id');
+                $form->text('rv_name')->required();
+                $form->image('rv_front_cover')->move('images/rv/' . date('Ym') . '/frontCover')->uniqueName()->maxSize(1024)->rules('mimes:jpg,jpeg,png,gif')->required();
+                $form->select('rv_series_id')->options(RvSeries::pluck('rv_series_name', 'id'))->required();
+
+                $form->checkbox('attachment_id')->options(RvAttachment::orderBy('order')->pluck('attachment_name', 'id'))->canCheckAll();
+
+                if ($form->isCreating()) {
+                    $val_status = 0;
+                } else {
+                    $val_status = $form->model()->stock;
+                }
+                $form->display('stock')->value($val_status);
+                $form->number('bed_count');
+                $form->currency('base_price')->symbol('$');
+
+                $form->editor('rv_discription')->options(['menubar' => false, 'toolbar' => ['code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | \
             styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | \
             table image media charmap emoticons hr pagebreak insertdatetime print preview | fullscreen | bdmap indent2em lineheight formatpainter axupimgs']])->imageDirectory('editor/images');
 
-            $form->display('created_at');
-            $form->display('updated_at');
+                $form->display('created_at');
+                $form->display('updated_at');
+            })->tab('Amount', function (Form $form) {
+
+                $form->array('rv_rent_setting', function ($form) {
+                    $form->fieldset('方案', function (NestedForm $form) {
+                        $form->select('week', __('起始星期'))->options([0 => '星期日', 1 => '星期一', 2 => '星期二', 3 => '星期三', 4 => '星期四', 5 => '星期五', 6 => '星期六']);
+                        $form->time('get', __('取車時間'))->format('HH:00');
+                        $form->time('back', __('還車時間'))->format('HH:00');
+                        $form->number('day', __('可租天數(夜)'));
+                        $form->currency('rental', __('租金費率/天(夜)'))->symbol('$');
+                        $form->currency('overtime', __('超租加收/天(夜)'))->symbol('$');
+                        $form->table('other_price', __('其他收費'), function ($table) {
+                            $table->column(12, function (NestedForm $form) {
+                                $form->text('item', __('項目'));
+                                $form->currency('price', __('價格'))->symbol('$');
+                                $form->select('type', __('計算方式'))->options(['night' => '天', 'times' => '次'])->default('night');
+                            });
+                        })->saving(function ($v) {
+                            return json_encode($v);
+                        });
+                        $form->currency('milage', __('里程數/夜'))->symbol('km');
+                        $form->text('note', __('備註'));
+                        // $form->switch('plan', __(''));
+                        $form->radio('plan', __('設為方案'))
+                            ->options([1 => '是', 0 => '否'])
+                            ->default(0)
+                            ->when(1, function (NestedForm $form) {
+                                $form->text('plan_title', __('方案名稱'));
+                            });
+                    })->collapsed();
+
+
+                })->saveAsJson();
+            });
 
             $form->deleting(function (Form $form) {
                 $data = $form->model()->toArray();
@@ -169,7 +178,6 @@ class RvModelInfoController extends AdminController
                 if (count($countRvVehicle) != 0) {
                     return $form->response()->error('数据受保護，不可刪除，删除失败');
                 }
-
             });
         });
     }
