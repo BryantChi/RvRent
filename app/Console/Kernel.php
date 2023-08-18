a22f4fcd1a858591a5c678eb5ec33ad6c9d8a48f
BryantChi <Bryantchi.work@gmail.com>
2023-08-19 03:00:30 +0800

    [MOD] 更改排程頻率時間

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('stock:dataclear')->everyMinute();
+        $schedule->command('stock:dataclear')->everyTenMinutes();
     }
 
     /**
