531f29649bf0f4721e20ecf017a7505b1cb3e247
BryantChi <Bryantchi.work@gmail.com>
2023-08-19 02:47:31 +0800

    [MOD] CronJob 設定

---
+++ app/Console/Commands/DataReconciliationCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\RentOrderInfo as Order;
+
+class DataReconciliationCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'stock:dataclear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Order&Stock DataReconciliation';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // return 0;
+        Order::setDataReconciliation();
+        \Log::info('Cron Job has been executed.');
+    }
+}
